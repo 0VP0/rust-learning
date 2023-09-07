f62bd876826cb3a6ada43d9754782d66ec9701c6
0VP0 <ahmad.j.alf@gmail.com>
2023-09-08 02:43:14 +0300

    fix: .gitignore file issue

--- target/debug/deps/rust_learning-aba9e9af41fa3f93.d
+++
@@ -1,5 +0,0 @@
-/home/ahmed/Desktop/rust-learning/target/debug/deps/rust_learning-aba9e9af41fa3f93.rmeta: src/main.rs
-
-/home/ahmed/Desktop/rust-learning/target/debug/deps/rust_learning-aba9e9af41fa3f93.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/rust_learning-bd40e1659506e32d.d
+++
@@ -1,5 +0,0 @@
-/home/ahmed/Desktop/rust-learning/target/debug/deps/rust_learning-bd40e1659506e32d.rmeta: src/main.rs
-
-/home/ahmed/Desktop/rust-learning/target/debug/deps/rust_learning-bd40e1659506e32d.d: src/main.rs
-
-src/main.rs:
