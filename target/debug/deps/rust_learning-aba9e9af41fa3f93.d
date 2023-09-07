ff76888c36108a2ea762f8fcd4cf33b390547e07
VP0 <ahmad.j.alf@gmail.com>
2023-09-08 02:01:57 +0300

    chore: add cargo

---
+++ target/debug/deps/rust_learning-aba9e9af41fa3f93.d
@@ -0,0 +1,5 @@
+/home/ahmed/Desktop/rust-learning/target/debug/deps/rust_learning-aba9e9af41fa3f93.rmeta: src/main.rs
+
+/home/ahmed/Desktop/rust-learning/target/debug/deps/rust_learning-aba9e9af41fa3f93.d: src/main.rs
+
+src/main.rs:
